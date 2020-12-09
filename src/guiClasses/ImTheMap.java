e76f1b2368688f081dc5670f3b3800b626b08605
Michael <michael.li.web@gmail.com>
2020-12-09 15:31:11 -0500

    Give credit to Haversine formula and round distance

--- src/guiClasses/ImTheMap.java
+++ src/guiClasses/ImTheMap.java
@@ -5,9 +5,11 @@ import java.io.*;
 import java.util.*;
 
 public class ImTheMap {
+    final static double AVERAGE_RADIUS_OF_EARTH_KM = 6371;
+    
     public static void main(String[] args) throws IOException {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter a postal code: ");
+        System.out.print("Enter a postal code: "); //L6E1G4
         String code = in.next();
         double x = 0, y = 0;
 
@@ -21,7 +23,7 @@ public class ImTheMap {
         for (int i=0;i<arr.length;i++) {
             //parsing JSON into something I can read
             arr[i] = arr[i].replaceAll("[^a-z0-9:.-]", ""); //ReGeX op
-            //tldr; ^ replaces everything that isn't a to z, 0 to 9, :, ., - with "" aka nothing
+            //tldr; the "[^a-z0-9:.-]" replaces everything that isn't a to z, 0 to 9, :, ., - with "" aka nothing
 
             if(arr[i].startsWith("longt")) {
                 System.out.println(arr[i]);
@@ -34,17 +36,17 @@ public class ImTheMap {
         }
         //lat = N/S
         //long = E/W
-        System.out.println("Boss to Carleton: "+calculateDistanceInKilometer(y, x, 45.3876, -75.6960));
+        System.out.println("Boss to Carleton: "+calculateDistance(y, x, 45.3876, -75.6960)+"km");
     }
-    public final static double AVERAGE_RADIUS_OF_EARTH_KM = 6371;
-    static double calculateDistanceInKilometer(double userLat, double userLng,
-                                            double venueLat, double venueLng) {
-        double latDistance = Math.toRadians(userLat - venueLat);
-        double lngDistance = Math.toRadians(userLng - venueLng);
+    // Haversine formula orz
+    static double calculateDistance(double y1, double x1, double y2, double x2) {
+        double latDistance = Math.toRadians(y1 - y2);
+        double lngDistance = Math.toRadians(x1 - x2);
         double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
-                + Math.cos(Math.toRadians(userLat)) * Math.cos(Math.toRadians(venueLat))
+                + Math.cos(Math.toRadians(y1)) * Math.cos(Math.toRadians(y2))
                 * Math.sin(lngDistance / 2) * Math.sin(lngDistance / 2);
         double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        return (Math.round(AVERAGE_RADIUS_OF_EARTH_KM * c));
+        double scale = 100;
+        return Math.round(AVERAGE_RADIUS_OF_EARTH_KM * c * scale) / scale;
     }
 }
