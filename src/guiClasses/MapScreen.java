e414a864c9d51bbca553e3955582ec523664a6ae
Michael <michael.li.web@gmail.com>
2020-12-11 10:49:46 -0500

    Add button to switch panel visibilities

--- src/guiClasses/MapScreen.java
+++ src/guiClasses/MapScreen.java
@@ -6,10 +6,12 @@ import javax.swing.event.DocumentListener;
 import javax.swing.event.UndoableEditListener;
 import javax.swing.text.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-public class MapScreen {
+public class MapScreen implements ActionListener {
     public static void main(String[] args) {
         new MapScreen();
     }
@@ -18,13 +20,16 @@ public class MapScreen {
     private JPanel coordPanel = new JPanel();
     private JLabel map = new JLabel();
     private JLabel circle = new JLabel();
+    private JTextArea text = new JTextArea();
+    private JButton go = new JButton("GO");
     private ImageIcon mapeh = new ImageIcon(new ImageIcon("./res/map.png").getImage().getScaledInstance(600, 360, 0));
     private Color bg = Color.decode("#072540");
     private Color highlight = Color.decode("#9C4668");
     private Color strongHighlight = Color.decode("#FF8AE2");
     private Border mapBorder = BorderFactory.createLineBorder(strongHighlight, 10);
+    private int x = -1, y = -1;
+    private boolean reveal = false;
     public MapScreen() { //920x610
-        setupPanel();
         setupMap();
         setupFrame();
         frame.repaint();
@@ -36,28 +41,6 @@ public class MapScreen {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     void setupMap() {
-        map = new JLabel();
-        map.setIcon(mapeh);
-        map.setBounds(80, 100, 600, 360);
-        map.setBorder(mapBorder);
-        map.addMouseListener(new MouseListener() {
-            @Override
-            public void mouseClicked(MouseEvent e) {}
-            @Override
-            public void mousePressed(MouseEvent e) {
-                System.out.println("SDFJKLSD"+e.getX()+" "+e.getY());
-                circle.setBounds(map.getX()+e.getX()-25, map.getY()+e.getY()-25, 50, 50);
-            }
-            @Override
-            public void mouseReleased(MouseEvent e) {}
-            @Override
-            public void mouseEntered(MouseEvent e) {}
-            @Override
-            public void mouseExited(MouseEvent e) {}
-        });
-        mapPanel.add(map);
-    }
-    void setupPanel() {
         mapPanel.setLayout(null);
         mapPanel.setBackground(bg);
         frame.add(mapPanel);
@@ -85,7 +68,6 @@ public class MapScreen {
         otherText.setBorder(border);
         mapPanel.add(otherText);
 
-        JButton go = new JButton("GO");
         go.setFont(new Font("Tahoma", Font.PLAIN, 32));
         go.setBounds(800, 500, 50, 50);
         go.setForeground(highlight);
@@ -93,10 +75,9 @@ public class MapScreen {
         go.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
         go.setBorderPainted(false);
         go.setFocusPainted(false);
-        go.set
+        go.addActionListener(this);
         mapPanel.add(go);
 
-        JTextArea text = new JTextArea();
         text.setFont(new Font("Tahoma", Font.PLAIN, 18));
         text.setBounds(400, 500, 100, 25);
         text.setForeground(highlight);
@@ -107,15 +88,52 @@ public class MapScreen {
 
         circle.setIcon(new ImageIcon(new ImageIcon("./res/circle.png").getImage().getScaledInstance(50, 50, 0)));
         mapPanel.add(circle);
-
+        map = new JLabel();
+        map.setIcon(mapeh);
+        map.setBounds(80, 100, 600, 360);
+        map.setBorder(mapBorder);
+        map.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {}
+            @Override
+            public void mousePressed(MouseEvent e) {
+                System.out.println("SDFJKLSD"+e.getX()+" "+e.getY());
+                circle.setBounds(map.getX()+e.getX()-25, map.getY()+e.getY()-25, 50, 50);
+                x = map.getX()+e.getX()-25;
+                y = map.getY()+e.getY()-25;
+            }
+            @Override
+            public void mouseReleased(MouseEvent e) {}
+            @Override
+            public void mouseEntered(MouseEvent e) {}
+            @Override
+            public void mouseExited(MouseEvent e) {}
+        });
+        mapPanel.add(map);
     }
-
     //this overrides the default Document to set a limit on how many characters can be in a JTextArea
     static class TextFieldLimit extends PlainDocument {
         @Override
         public void insertString(int offset, String str, AttributeSet attr) throws BadLocationException {
             if((getLength()+str.length())<=6) {
                 super.insertString(offset, str, attr);
+            }
+        }
+    }
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        if(event.getSource()==go) {
+            if(!text.getText().equals("A1B2C3") && text.getText().length()==6) {
+                mapPanel.setVisible(reveal);
+                coordPanel.setVisible(!reveal);
+
+                reveal = !reveal;
+            }
+            else if(x!=-1 && y!=-1) {
+                mapPanel.setVisible(reveal);
+                coordPanel.setVisible(!reveal);
+
+                reveal = !reveal;
             }
         }
     }
